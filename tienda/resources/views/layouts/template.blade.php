3f07787117705fe992e04de8803571dfc8ebebbf
jost3 <1427593@senati.pe>
2023-10-29 09:54:59 -0500

    cambio de rutas y agregacion del login en la pagina princiapl

--- tienda/resources/views/layouts/template.blade.php
+++ tienda/resources/views/layouts/template.blade.php
@@ -17,6 +17,9 @@
         </style>
     </head>
     <body>
+    <div style="text-align: right;">
+    <a href="{{ route('productos.index') }}" class="btn btn-primary">INICAR SESION</a>
+    </div>
         <header>
             <h1 class="site-heading text-center text-faded d-none d-lg-block">
                 <span class="site-heading-upper text-primary mb-3">Tienda Virtual</span>
@@ -30,9 +33,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto">
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">Home</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="{{ url('/') }}">Home</a></li>
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">About</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="{{ url('/products') }}">Products</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">Products</a></li>
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">Store</a></li>
                     </ul>
                 </div>
